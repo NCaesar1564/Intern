825a97101a68aa703a3bf2c482612e92086af122
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-02 13:19:05 +0700

    'Intern'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,20 @@
 import Head from "next/head";
 import User from "@/app/Container/User";
 
+export const metadata = {
+  openGraph: {
+    images: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Lotus_flower_%28978659%29.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta property="og:title" content="Bài viết nổi bật" />
-        <meta property="og:description" content="Danh sách các bài viết mới nhất" />
-        <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/f/f5/Lotus_flower_%28978659%29.jpg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:type" content="article" />
-      </Head>
       <User />
     </>
   );
