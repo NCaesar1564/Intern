ed314886fe5665a0535e143ee97611ad22fe851b
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-02 13:24:48 +0700

    'Intern'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
+"use client";
 import User from "@/app/Container/User";
 
-export const metadata = {
-  openGraph: {
-    images: [
-      {
-        url: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Lotus_flower_%28978659%29.jpg',
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
 export default function Home() {
   return (
     <>
