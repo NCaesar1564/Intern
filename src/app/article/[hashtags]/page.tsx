db6fdd9739069ffced255ec46805045344bba0da
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-03 16:19:38 +0700

    'Intern'

--- src/app/article/[hashtags]/page.tsx
+++ src/app/article/[hashtags]/page.tsx
@@ -1,4 +1,21 @@
+import { Metadata } from 'next';
 import ArticlePage from './ArticlePage'
+
+export const metadata: Metadata = {
+  title: "Trang chủ",
+  openGraph: {
+    url: "hashtags",
+    description: "description",
+    images: [
+      {
+        url: "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Alt text cho hình ảnh"
+      }
+    ]
+  }
+};
 export default function page() {
   console.log("Home component đang chạy trên:", typeof window === "undefined" ? "Server" : "Client");
 
