9de7c6b658cc68ef75c0df93b4526f752a8c8bde
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-03 14:07:03 +0700

    'Intern'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import UserWrapper from "@/app/Container/UserWrapper";
 import Head from "next/head";
+
 export default function Home() {
   console.log("Home component đang chạy trên:", typeof window === "undefined" ? "Server" : "Client");
+
   const siteUrl =
     process.env.NEXT_PUBLIC_SITE_URL ||
     (process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000");
+
   const ogImage = `${siteUrl}/Images/banner.png`;
+
   return (
     <>
-      <Head >
+      <Head>
         <meta property="og:image" content={ogImage} />
       </Head>
       <UserWrapper />
