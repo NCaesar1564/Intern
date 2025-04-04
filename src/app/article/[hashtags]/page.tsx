434df752d6f3f2225377bb9bdfe3d393a4a33c45
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-04 11:12:45 +0700

    'Intern'

--- src/app/article/[hashtags]/page.tsx
+++ src/app/article/[hashtags]/page.tsx
@@ -1,51 +1,39 @@
 import ArticlePage from './ArticlePage'
 import { Metadata, ResolvingMetadata } from "next";
 
-interface Article {
-  id: number;
-  idContent: number;
-  nameArticle: string;
-  imgArticle: string;
-  hashtags: string;
-  description?: string;
-  author: string;
-  content: string;
-  category: string;
-}
-
-type PageProps = {
-  params: { hashtags: string }
-  searchParams?: { [key: string]: string | string[] | undefined }
+type Props = {
+  params: Promise<{ hashtags: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export async function generateMetadata(
-  { params }: PageProps,
+  { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const hashtags = params.hashtags
-  const res = await fetch(`http://localhost:3000/data.json`);
-  const data = await res.json();
-  const articles: Article[] = data.articles;
-  const article = articles.find(a => a.hashtags === hashtags);
+  const hashtags = (await params).hashtags
 
-  if (!article) {
-    return {
-      title: "ZNew | Error",
-    };
-  }
+  const article = await fetch(`http://localhost:3000/article/${hashtags}`)
+    .then((res) =>
+      res.json()
+    )
 
   return {
-    title: `ZNew | ${article.nameArticle}`,
+    title: article.nameArticle,
+    description: article.description,
     openGraph: {
-      images: [{
-        url: `${article.imgArticle}`,
-        width: 1200,
-        height: 730,
-      }],
-    },
+      images: [
+        {
+          url: `${article.imgArticle}`,
+          height: 1200,
+          width: 720
+        }
+      ]
+    }
   }
 }
-export default function page() {
+
+
+export default function page({ params, searchParams }: Props) {
   console.log("Home component đang chạy trên:", typeof window === "undefined" ? "Server" : "Client");
   return (
     <>
