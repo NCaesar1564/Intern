5b30c5c51101a899c45d8b97fac15be67cc6a8a2
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-06 16:01:54 +0700

    'Intern'

--- src/app/article/[hashtags]/page.tsx
+++ src/app/article/[hashtags]/page.tsx
@@ -1,27 +1,22 @@
 import { Metadata } from 'next';
-import ArticleDetail from './ArticleDetail'; // Giả sử bạn đã có component ArticleDetail
+import ArticleDetail from './ArticleDetail'; 
 import axios from 'axios';
 
 type Props = {
   params: { hashtags: string };
 };
-
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { hashtags } = params;
-
-  // Fetch bài viết từ API dựa trên hashtag
   const res = await axios.get(`http://localhost:4000/articles`);
   const article = res.data.find((a: { hashtags: string }) => a.hashtags === hashtags);
-
   if (!article) {
-    // Nếu không tìm thấy bài viết, trả về metadata mặc định
+    
     return {
       title: 'Bài viết không tồn tại',
       description: 'Không tìm thấy bài viết yêu cầu',
     };
   }
 
-  // Trả về metadata từ dữ liệu bài viết
   return {
     title: article.nameArticle,
     description: article.description,
