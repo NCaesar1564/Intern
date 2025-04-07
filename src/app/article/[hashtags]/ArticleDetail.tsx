55de3de528ad6d50fce43ccb1a27cb9ee17ff0a5
NCaesar <nguyenthiennhan1564@gmail.com>
2025-04-07 18:48:26 +0700

    'Intern'

--- src/app/article/[hashtags]/ArticleDetail.tsx
+++ src/app/article/[hashtags]/ArticleDetail.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 
 
 
-export default function ArticleDetail() {
+export default function ArticleDetail({ hashtags }: { hashtags: string }) {
 
   interface Article {
     id: number;
@@ -20,7 +20,7 @@ export default function ArticleDetail() {
   }
 
   const params = useParams();
-  const hashtags = params.hashtags as string;
+  hashtags = params.hashtags as string;
   const [articles, setArticles] = useState<Article | null>(null);
 
   useEffect(() => {
